c48ddd2d9b7159f05cc0f23931736617db9c8cea
Enmar123 <js_enmar1@hotmail.com>
2019-01-28 01:25:46 -0500

    v0.0.1dev - In development

--- gravipy.py
+++ gravipy.py
@@ -13,30 +13,90 @@ Code diagram:
       |
     Universe
         Particle
-            Vector
+            Vector <- not really needed
 """
+import numpy as np
+import cv2 as cv
+import time
+
 # Screen object is a viewport into our universe and will always
 # accomodate the size of the universe.
 class Screen:
+    def __init__(self):
+        self.universe = None
+        self.fps = 35           # fps of the universe
     pass
 
 # Universe object contains real distances, particles, an dictates
 # the rate of time within our system.
 class Universe:
     def __init__(self):
-        self.size = None
+        self.size = (500000, 500000)    # m
+        self.G = 6.674 * 10**(-11)      # N*(m/kg)**2
+        self.timestep = 0.1             # update rate
+        self.rate = 1                  # speed of time
+        self.simtime = 0
+        
+        self.particles = []             # list of particles
+    
+    def update(self):
+        # <TODO>
+        # Create updating function for the universe
+        timepassed = self.timestep * self.rate
+        
+        
+        # forces should be calculated at the start of the timestep
+        self.interact()
+        # physics should be calculated at the end of the timestep
+        self.physics()
+        
+        
+        time.sleep(timepassed)
+        self.simtime += timepassed
+        
+        
+        
+    # Pits particle1 against every other particle in the universe
+    # so that interactions can be calculated.
+    def interact(self):
+        for particle1 in self.particles:
+            for particle2 in self.particles:
+                if particle1 is particle2 :
+                    pass #skip interaction
+                else:
+                    # specify interactions
+                    self.gravity(particle1, particle2)
     
     
-    def gravity(self):
+    def gravity(self, particle1, particle2):
         """
         Updates the acceleration vectors of every particle.
         """
-        pass
+        p1, p2 = particle1, particle2
+        m1, m2 = p1.mass, p2.mass
+        r = self.get_dist(p1, p2)
+        F = self.G * (m1 * m2) / r**2   # Newtons law of gravity
+        p1.apply_force(F, direction)
+        
+    
+    def get_dist(self, particle1, particle2):
+        p1, p2 = particle1, particle2
+        dist = np.sqrt(np.sum(np.square(p1.pos - p2.pos)))
+        return dist
 
     def physics(self):
         """
         Updates velocity and position of particles  
         """
+    def spawn_particle(self, mass=0, pos=[0,0,0], 
+                                     vel=[0,0,0],
+                                     acc=[0,0,0]):
+        particle = Particle()
+        particle.mass = mass
+        particle.pos = np.array(pos)
+        particle.vel = np.array(vel)
+        particle.acc = np.array(acc)
+        return particle
 
 
 # Particle objects describe themselves int the 
@@ -47,6 +107,9 @@ class Particle:
         
         self.vel = None
         self.acc = None
+        
+    def apply_force(self, F):
+        
         
 
         
@@ -59,6 +122,6 @@ class Vector:
         self.y = None
         self.z = None
         
-def make_particle():
-    particle = Particle()
-    particle.mass = 10
+    def vector_sum(self):
+        pass
+        
