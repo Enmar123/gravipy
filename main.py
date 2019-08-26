f2ab1c10eaa646969cd3f961f874dc8cb010eb9f
Enmar123 <js_enmar1@hotmail.com>
2019-08-26 00:01:37 -0400

    Can project point objects onto the screen

--- main.py
+++ main.py
@@ -24,21 +24,42 @@ class Point:
         
     def get_pos(self):
         return list(self.pos)
+    
+class Screen:
+    def __init__(self, size):
+        self.size = size
+        self.img = np.zeros((size,size,3), np.uint8)
+    
+    def project(self, points):
+        pts = []
+        for point in points:
+            pts.append(list(np.round(point.get_pos()).astype(np.int)))
+        return pts
+    
+    def draw_pts(self, pts):
+        for pt in pts:
+            self.img[pt[0]%self.size,pt[1]%self.size] = [255,255,255]
+            
+    def clear(self):
+        self.img = np.zeros((self.size,self.size,3), np.uint8)
+        
         
 if __name__=="__main__":
-    blankimg = np.zeros((500,500,3), np.uint8)
-    
-    point1 = Point([255,255],[1,0],[1,0])
-    point2 = Point([255,500],[-1,0],[0,0])
+    screen = Screen(500)
+    point1 = Point([255,255],[1,0],[0.5,0])
+    point2 = Point([255,300],[1,1],[0.1,0])
+    #point2 = Point([255,500],[-1,0],[0,0])
     
     for i in range(500):
-        img = np.zeros((500,500,3), np.uint8)
+        screen.clear()
 
         point1.update(1)
-        img[point1.get_pos()[0]%500,point1.get_pos()[1]%500] = [255,255,255]
-        #img[255,255] = [255,255,255]
+        point2.update(1)
+        points = [point1, point2]
+        pts = screen.project(points)
+        screen.draw_pts(pts)
         
-        cv.imshow("Screen", img)
+        cv.imshow("Screen", screen.img)
         if cv.waitKey(int(1000/25)) == ord('q'): # Press q to stop sim
             break
     cv.destroyAllWindows()
