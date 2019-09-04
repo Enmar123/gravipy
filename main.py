85773b45480010bffdf7ecc3de749be96887953f
Enmar123 <js_enmar1@hotmail.com>
2019-09-03 22:44:58 -0400

    gravity and optional bounds

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ Created on Sun Aug 25 03:03:02 2019
 """
 import numpy as np
 import cv2 as cv
+import math as ma
 import time
+import copy
 
 
 
-    
+       
     
 class Point:
     def __init__(self, pos, vel, acc):
@@ -24,6 +26,81 @@ class Point:
         
     def get_pos(self):
         return list(self.pos)
+    
+class Particle(Point):
+    def __init__(self, pos, vel, acc, mass):
+        Point.__init__(self, pos, vel, acc)
+        self.mass = mass
+        self.force = [0,0]
+        self.forces = [[0,0]]
+        
+    def update(self):
+        if len(self.forces) <= 1:
+            self.force = self.forces[0]
+        else:
+            self.force = np.add.reduce(self.forces)
+        self.forces = [np.array([0,0])]
+        self.acc = np.array(self.force)/self.mass
+        Point.update(self, 1)
+        
+        
+    def apply_force(self, force):
+        self.forces.append(force)
+        
+    def get_mass(self):
+        return self.mass
+
+        
+class Universe():
+    def __init__(self):
+        self.G = 1
+        
+        self.particles = []
+        
+        self.bounded = True
+       
+    def update(self):
+        """ Updates the forces and properties of particles in the universe """
+        self.gravity()
+        self.update_motion()
+   
+    def gravity(self):
+        for particle1 in self.particles:
+            for particle2 in self.particles:
+                if particle1 == particle2:
+                    pass
+                else:
+                    m1 = particle1.get_mass()
+                    m2 = particle2.get_mass()
+                    r = self.get_dist(particle1, particle2)
+                    F = self.G*m1*m2/r**2
+                    x, y = np.array(particle2.get_pos()) - np.array(particle1.get_pos())
+                    Fx = F * np.cos(np.arccos(x/r))
+                    Fy = F * np.sin(np.arcsin(y/r))
+                    particle1.apply_force(np.array([Fx,Fy]))
+                    
+   
+    def update_motion(self):
+        [particle.update() for particle in self.particles]
+        if self.bounded:
+            for particle in self.particles:
+                x, y = particle.get_pos()
+                if x<0 or x>500:
+                    particle.vel[0] = particle.vel[0]*-1
+                if y<0 or y>500:
+                    particle.vel[1] = particle.vel[1]*-1
+                
+    def get_dist(self, p1, p2):
+       xydist = np.array(p1.get_pos()) - np.array(p2.get_pos())
+       rdist = np.hypot(xydist[0], xydist[1])
+       return rdist
+   
+    def add_prt(self, particles):
+        if type(particles) is list:
+            [self.particles.append(particle) for particle in particles]
+        else:
+            self.particles.append(particles)
+    
     
 class Screen:
     def __init__(self, size):
@@ -46,20 +123,29 @@ class Screen:
         
 if __name__=="__main__":
     screen = Screen(500)
-    point1 = Point([255,255],[1,0],[0.5,0])
-    point2 = Point([255,300],[1,1],[0.1,0])
-    #point2 = Point([255,500],[-1,0],[0,0])
+    uni = Universe()
+    #point1 = Point([255,255],[1,0],[0.5,0])
+    #point2 = Point([255,300],[1,1],[0.1,0])
+    #particle1 = Particle([255,255],[0,0.3],[0,0],20)
+    #particle2 = Particle([200,255],[0,-.3],[0,0],20)
+    #particle1 = Particle([255,255],[0,-.15],[0,0],100) # @500
+    #particle2 = Particle([200,255],[0,1.5],[0,0],10)   # @
+    particle1 = Particle([255,255],[-1,0],[0,0],100) # @500
+    particle2 = Particle([200,255],[0,3],[0,0],10)   # @500
+    uni.add_prt([particle1, particle2])
     
-    for i in range(500):
+    
+    for i in range(2000):
         screen.clear()
 
-        point1.update(1)
-        point2.update(1)
-        points = [point1, point2]
-        pts = screen.project(points)
+        uni.update()
+        #point1.update(1)
+        #point2.update(1)
+        #points = [point1, point2]
+        pts = screen.project(uni.particles)
         screen.draw_pts(pts)
         
         cv.imshow("Screen", screen.img)
-        if cv.waitKey(int(1000/25)) == ord('q'): # Press q to stop sim
+        if cv.waitKey(int(1000/500)) == ord('q'): # Press q to stop sim
             break
     cv.destroyAllWindows()
